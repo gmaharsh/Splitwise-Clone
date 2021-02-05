c758754391d9559e0d9c64e478d50d6bfd150385
Maharsh <gmaharsh@yahoo.com>
2021-02-05 11:24:46 -0600

    Created Account model

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config');
 
 const typeDefs = gql`
     type Query{
@@ -16,6 +19,18 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+})
+
+
+//Database Connection
+mongoose.connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("Conneted to Database")
+}).catch((err) => {
+    console.log("Err:-", err)
 })
 
 server.listen(5000)
---
+++ models/Account.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require('mongoose');
+
+const AccountSchema = new Schema({
+    username: String,
+    body: String,
+    createdAt:String,
+    amountOwe: [
+        {
+            amount: Integer,
+            username: String,
+            createdAt: String
+        }
+    ],
+    amountOwed: [
+        {
+            amount: Integer,
+            username: String,
+            createdAt: String
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref:'users'
+    }
+})
