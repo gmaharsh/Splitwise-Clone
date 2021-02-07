3718d3b6da5d442b1a0542b40fef694195de1f09
Maharsh <gmaharsh@yahoo.com>
2021-02-07 12:48:46 -0600

    Sidebar Component Basic UI Created

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Header from './Components/Header/Header';
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
+import Sidebar from './Components/Body/Sidebar/Sidebar';
+import Dashboard from './Components/Body/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Header />
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          {/* <Dashboard /> */}
+        </div>
       </Router>
     </div>
   );
---
+++ client/src/Components/Body/Sidebar/Sidebar.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__upper">
+                <div className="sidebar__upper__items">
+                    <span>
+                        <img src="https://dev.splitwise.com/images/logo.svg" alt="" />
+                    </span>
+                    <h3>Dashboard</h3>
+                </div>
+                <div className="sidebar__upper__items">
+                    <span>
+                        <i class="flag outline icon"></i>
+                    </span>
+                    <h3>Recent Items</h3>
+                </div>
+                <div className="sidebar__upper__items sidebar__upper__items__iput">
+                    <span>
+                        <i class="search icon" style={{ backgroundColor:"#eeeeee"}}></i>
+                    </span>
+                    
+                    <input type="text" placeholder="Filter By Name"/>
+                </div>
+                <div className="sidebar__upper__items">
+                    <span>
+                        <i class="list icon"></i>
+                    </span>
+                    <h3>All Expenses</h3>
+                </div>
+            </div>
+            <div className="sidebar__middle">
+                <div className="sidebar__headline">
+                    <h4>GROUPS</h4>
+                    <span>
+                        <i class="plus icon"></i>
+                        Add
+                    </span>
+                </div>
+                <div className="sidebar__items">
+                    <i class="tag icon"></i>
+                    <h3>Home</h3>
+                </div>
+                <div className="sidebar__items">
+                    <i class="tag icon"></i>
+                    <h3>Friends</h3>
+                </div>
+            </div>
+            <div className="sidebar__lower">
+                <div className="sidebar__headline">
+                    <h4>FRIENDS</h4>
+                    <span>
+                        <i class="plus icon"></i>
+                        Add
+                    </span>
+                </div>
+                <div className="sidebar__items">
+                    <i class="user icon"></i>
+                    <h3>Elon Musk</h3>
+                </div>
+                <div className="sidebar__items">
+                    <i class="user icon"></i>
+                    <h3>Mark</h3>
+                </div>
+                <div className="sidebar__items">
+                    <i class="user icon"></i>
+                    <h3>Sundar Pichai</h3>
+                </div>
+                <div className="sidebar__items">
+                    <i class="user icon"></i>
+                    <h3>Mary Barra</h3>
+                </div>
+                <div className="sidebar__items">
+                    <i class="user icon"></i>
+                    <h3>Jack Ma</h3>
+                </div>
+                <div className="sidebar__items">
+                    <i class="user icon"></i>
+                    <h3>Susan Wojcicki</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
