5ecb74a05e358c5b9c5dc985e209ecbbc223ff13
Maharsh <gmaharsh@yahoo.com>
2021-02-08 16:39:54 -0600

    Created Modal

--- client/src/Components/Body/Dashboard/Dashboard.js
+++ client/src/Components/Body/Dashboard/Dashboard.js
@@ -1,16 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Dropdown, Header, Image, Modal } from 'semantic-ui-react';
 import Card from './Card/Card';
 import './Dashboard.css';
 
 function Dashboard() {
+
+    const [open, setOpen] = useState(false);
+
+    const addExpense = () => {
+        setOpen(false)
+    }
+
+    const friendOptions = [
+        {
+            key: 'Jenny Hess',
+            text: 'Jenny Hess',
+            value: 'Jenny Hess',
+            image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
+        },
+        {
+            key: 'Elliot Fu',
+            text: 'Elliot Fu',
+            value: 'Elliot Fu',
+            image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
+        },
+        {
+            key: 'Stevie Feliciano',
+            text: 'Stevie Feliciano',
+            value: 'Stevie Feliciano',
+            image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
+        },
+    ]
+    
+
     return (
         <div className="dashboard">
             <div className="dashboard__heading">
                 <h2>Dashboard</h2>
                 <div className="dashboard__buttons">
-                    <button class="medium ui orange button">
+                    <Modal
+                        size='tiny'
+                        onClose={() => setOpen(false)}
+                        onOpen={() => setOpen(true)}
+                        open={open}
+                        trigger={<button class="medium ui orange button">
                         Add an Expense
-                    </button>
+                    </button>}
+                    >
+                    <Modal.Header>Add an Expense</Modal.Header>
+                    <Modal.Content className="model__content">
+                            With you and :
+                             <input
+                                placeholder="Enter your friend's name"
+                            />
+                    </Modal.Content>
+                    <hr />
+                    <Modal.Content className="model__contentdescription divider">
+                            <input
+                                placeholder="Enter a description"
+                            />
+                            <input
+                                placeholder="Enter a amount"
+                            />
+                    </Modal.Content>
+                    <Modal.Content className="model__contentbuttons">
+                            Paid by 
+                            <Dropdown
+                                text='Add user'
+                                labeled
+                                button
+                                className='icon'
+                                style={{ marginLeft: '10px' }}
+                            >
+                                <Dropdown.Menu>
+                                <Dropdown.Header content='People You Might Know' />
+                                {friendOptions.map((option) => (
+                                    <Dropdown.Item key={option.value} {...option} />
+                                ))}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            and split
+                            <Dropdown
+                                text='Add user'
+                                labeled
+                                button
+                                className='icon'
+                                style={{ marginLeft: '10px' }}
+                            >
+                                <Dropdown.Menu>
+                                <Dropdown.Header content='People You Might Know' />
+                                {friendOptions.map((option) => (
+                                    <Dropdown.Item key={option.value} {...option} />
+                                ))}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                    </Modal.Content>    
+                    <Modal.Actions>
+                            <Button
+                                color='black'
+                                onClick={() => setOpen(false)}>
+                            Cancel
+                            </Button>
+                            <Button
+                                content="Save"
+                                onClick={addExpense}
+                                positive
+                            />
+                    </Modal.Actions>
+                    </Modal>
                     <button class="medium ui teal button">
                         Settle up
                     </button>
@@ -51,6 +148,7 @@ function Dashboard() {
                     </div>
                     <div className="dashboard__owed">
                         <Card name="Harshit" amount="700" owe="false" />
+                        <Card name="Rachana" amount="7010" owe="false" />
                     </div>
                 </div>
             </div>
